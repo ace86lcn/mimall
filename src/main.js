aba66367ff1392f5519d96130cfa08654f359677
lcn <ace86lcn@163.com>
2020-05-16 18:09:01 +0800

    错误拦截

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App'
 import router from './router'
 
+// 根据前端的跨域方式做调整
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
+// 错误接口拦截
+axios.interceptors.response.use(function (response) {
+  let res = response.data
+  // eslint-disable-next-line eqeqeq
+  if (res.status == 0) {
+    return res.data
+  // eslint-disable-next-line eqeqeq
+  } else if (res.status == 10) {
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
