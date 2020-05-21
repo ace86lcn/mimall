21a80257e71d96b3828253896ee6e9be5bc85d1b
lcn <ace86lcn@163.com>
2020-05-21 22:40:25 +0800

    nav-navbar

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,11 +1,63 @@
 <template>
-    <div>NavHeader</div>
+    <div class="header">
+      <div class="nav-topbar">
+        <div class="container">
+          <div class="topbar-menu">
+            <a href="javascript:;">小米商城</a>
+            <a href="javascript:;">MUI</a>
+            <a href="javascript:;">云服务</a>
+            <a href="javascript:;">协议规则</a>
+          </div>
+          <div class="topbar-user">
+            <a href="javascript:;">登录</a>
+            <a href="javascript:;">退出</a>
+            <a href="javascript:;">我的订单</a>
+            <a href="javascript:;" class="my-cart"><span class="icon-cart"></span>购物车</a>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 <script>
 export default {
   name: 'NavHeader'
 }
 </script>
-<style>
-
+<style lang='scss' scoped>
+  .header {
+    .nav-topbar {
+      height:39px;
+      line-height:39px;
+      background-color:#333333;
+      color:#B0B0B0;
+      .container {
+        width:1226px;
+        margin-right: auto;
+        margin-left: auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        a {
+          display:inline-block;
+          color:#B0B0B0;
+          margin-right:17px;
+        }
+        .my-cart {
+          width:110px;
+          background-color:#FF6600;
+          text-align:center;
+          color:#ffffff;
+          margin-right:0;
+          .icon-cart {
+            width: 16px;
+            height:12px;
+            display: inline-block;
+            background:url('../../static/imgs/icon-cart-checked.png') no-repeat;
+            background-size:contain;
+            margin-right:4px;
+          }
+        }
+      }
+    }
+  }
 </style>
